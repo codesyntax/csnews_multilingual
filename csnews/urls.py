4219b7e3953b4a751eb4b71f45b72ede9e5fce37
Urtzi Odriozola <uodriozola@codesyntax.com>
2013-03-26 12:40:58 +0100

    Update urls.py
    Comments feed url added

--- csnews/urls.py
+++ csnews/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import *
 
-from csnews.feeds import LatestNews
+from csnews.feeds import LatestNews, LatestComments
 #feed_dict = {'rss': LatestNews}
 
 urlpatterns = patterns('',
     (r'^$','csnews.views.index'),
     (r'^feed/$', LatestNews()),
+    (r'^comment-feed/$', LatestComments()),
     (r'^hemeroteka/', 'csnews.views.hemeroteka'),            
     (r'^(?P<article_slug>[\-\d\w]+)/$','csnews.views.article_index'),
 )
