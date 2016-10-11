5bb7786010b70a2aac09ce6caa8cbbc6ef541bfb
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-10-11 10:00:46 +0200

    Bugfix

--- csnews_multilingual/views.py
+++ csnews_multilingual/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from csnews_multilingual.models import Article
 from csnews_multilingual.diggpaginator import DiggPaginator
-from datetime import datetime
+from django.utils.translation import get_language
 
 ARTICLE_NUMBER_PER_PAGE = 20
 
 
 def _get_page(list, page):
-    """ """
     paginator = DiggPaginator(list, ARTICLE_NUMBER_PER_PAGE, body=5, padding=2)
     try:
         page = int(page)
@@ -24,13 +23,13 @@ def _get_page(list, page):
 
 
 def index(request):
-    articles = Article.objects.filter(is_public=True)
+    articles = Article.objects.language().filter(is_public=True)
     page = _get_page(articles, request.GET.get('page', '1'))
     return render_to_response('news/articles.html', locals(), context_instance=RequestContext(request))
 
 
 def article_index(request, article_slug):
-    obj = get_object_or_404(Article, slug=article_slug)
+    obj = get_object_or_404(Article, slug=article_slug, language=get_language())
     return render_to_response('news/article.html', locals(), context_instance=RequestContext(request))
 
 
