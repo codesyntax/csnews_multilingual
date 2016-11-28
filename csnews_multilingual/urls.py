36682b4a54b461d001b85187f5e60e233754fa34
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-11-28 10:09:22 +0100

    Preparing release 3.13

--- csnews_multilingual/urls.py
+++ csnews_multilingual/urls.py
@@ -7,8 +7,8 @@ from django.utils.translation import ugettext_lazy as _
 urlpatterns = patterns(
     'csnews_multilingual.views',
     url(r'^$', 'index', name='csnews_index'),
-    url(_(r'^tag/(?P<tag_slug>[\d\w]+)/$'), 'tag_index', name='csnews_tag'),
-    url(_(r'^feed/$'), LatestNews(), name='csnews_feed'),
-    url(_(r'^archive/$'), 'archive', name='csnews_archive'),
+    url(r'^tag/(?P<tag_slug>[\d\w]+)/$', 'tag_index', name='csnews_tag'),
+    url(r'^feed/$', LatestNews(), name='csnews_feed'),
+    url(r'^archive/$', 'archive', name='csnews_archive'),
     url(r'^(?P<article_slug>[\-\d\w]+)/$', 'article_index', name='csnews_article'),
 )
